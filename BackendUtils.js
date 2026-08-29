4f8fc0847e3fc2426cc75aa8066d87c840be4f01
battosaiazodev <primeroprofessionalego@gmail.com>
2026-08-29 22:19:54 +0200

    Update BackendUtils.js

--- BackendUtils.js
+++ BackendUtils.js
@@ -235,7 +235,7 @@ class UserModel {
       experience: 0,
       crowns: 0,
       hiddenRating: 0,
-      isBanned: true,
+      isBanned: false,
       banReason: "Cheating",
       bannedAt: new Date(),
       inventory: [{
